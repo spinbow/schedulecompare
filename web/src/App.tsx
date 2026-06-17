612a57483cc20695a72828dd80198c9430e04094
sylophi <dev@sylophi.com>
2026-06-16 19:37:20 -0700

    tsc and lefthook

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from './assets/vite.svg';
-import heroImg from './assets/hero.png';
-import './App.css';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -21,7 +21,11 @@ function App() {
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
         </div>
-        <button type="button" className="counter" onClick={() => setCount((count) => count + 1)}>
+        <button
+          type="button"
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
           Count is {count}
         </button>
       </section>
@@ -59,7 +63,11 @@ function App() {
           <ul>
             <li>
               <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg className="button-icon" role="presentation" aria-hidden="true">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
                   <use href="/icons.svg#github-icon"></use>
                 </svg>
                 GitHub
@@ -67,7 +75,11 @@ function App() {
             </li>
             <li>
               <a href="https://chat.vite.dev/" target="_blank">
-                <svg className="button-icon" role="presentation" aria-hidden="true">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
                   <use href="/icons.svg#discord-icon"></use>
                 </svg>
                 Discord
@@ -75,7 +87,11 @@ function App() {
             </li>
             <li>
               <a href="https://x.com/vite_js" target="_blank">
-                <svg className="button-icon" role="presentation" aria-hidden="true">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
                   <use href="/icons.svg#x-icon"></use>
                 </svg>
                 X.com
@@ -83,7 +99,11 @@ function App() {
             </li>
             <li>
               <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg className="button-icon" role="presentation" aria-hidden="true">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
                   <use href="/icons.svg#bluesky-icon"></use>
                 </svg>
                 Bluesky
