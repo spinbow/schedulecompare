953d90192cc7552e15183ec6ff26f0104ec5a9e3
sylophi <dev@sylophi.com>
2026-06-17 06:21:06 -0700

    better cors handling

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,28 +1,10 @@
 import { createHTTPServer } from '@trpc/server/adapters/standalone';
 import { appRouter } from './app-router';
-
-const corsHeaders = {
-  'Access-Control-Allow-Origin': 'http://localhost:5173',
-  'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
-  'Access-Control-Allow-Headers': 'content-type,authorization,trpc-accept',
-  Vary: 'Origin',
-};
+import cors from 'cors';
 
 const server = createHTTPServer({
   router: appRouter,
-  middleware(req, res, next) {
-    for (const [key, value] of Object.entries(corsHeaders)) {
-      res.setHeader(key, value);
-    }
-
-    if (req.method === 'OPTIONS') {
-      res.statusCode = 204;
-      res.end();
-      return;
-    }
-
-    next();
-  },
+  middleware: cors(),
 });
 
 server.listen(3000);
