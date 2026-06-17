2d99e74eed86659c3f157740ce4d5c79dd29c8f8
sylophi <dev@sylophi.com>
2026-06-17 06:37:43 -0700

    tanstack query

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,44 +1,13 @@
-import { createTRPCClient, httpBatchLink } from '@trpc/client';
-import { useEffect, useState } from 'react';
-import type { AppRouter } from '@schedulecompare/api';
-import type { User } from '@schedulecompare/shared';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { TestFetch } from './components/test-fetch';
 
-const trpc = createTRPCClient<AppRouter>({
-  links: [
-    httpBatchLink({
-      url: 'http://localhost:3000',
-    }),
-  ],
-});
+const queryClient = new QueryClient();
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [fetchedData, setFetchedData] = useState<User | null>(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      console.log('fetching...');
-      const data = await trpc.userList.query();
-      console.log(`got data:`, data[0]);
-      setFetchedData(data[0]);
-    };
-    fetchData();
-  }, []);
-
   return (
-    <>
-      <button type="button" onClick={() => setCount((count) => count + 1)}>
-        Count is {count}
-      </button>
-      {fetchedData ? (
-        <div>
-          <p>{`ID: ${fetchedData.id}`}</p>
-          <p>{`Name: ${fetchedData.name}`}</p>
-        </div>
-      ) : (
-        <p>FETCHING...</p>
-      )}
-    </>
+    <QueryClientProvider client={queryClient}>
+      <TestFetch />
+    </QueryClientProvider>
   );
 }
 
---
+++ web/src/lib/trpc.ts
@@ -0,0 +1,10 @@
+import type { AppRouter } from '@schedulecompare/api';
+import { createTRPCClient, httpBatchLink } from '@trpc/client';
+
+export const trpc = createTRPCClient<AppRouter>({
+  links: [
+    httpBatchLink({
+      url: 'http://localhost:3000',
+    }),
+  ],
+});
